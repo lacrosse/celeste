433fe9631434a3b6fa7837c76d48c14317f437e0
Alexander Skiba <alex@foremal.com>
2017-02-14 22:44:06 +0700

    Refactor API.AssemblageView

--- web/views/api/assemblage_view.ex
+++ web/views/api/assemblage_view.ex
@@ -3,76 +3,98 @@ defmodule Celeste.API.AssemblageView do
 
   def render("index.json", %{assemblages: assemblages}) do
     %{
-      assemblages: assemblages |> Enum.map(&short(&1))
+      assemblages: assemblages |> Enum.map(&render_partial(&1, %{}))
     }
   end
 
   def render("short_show.json", %{assemblage: assemblage}) do
-    %{
-      assemblage: short(assemblage)
-    }
+    %{assemblage: render_partial(assemblage, %{})}
   end
 
   def render("show.json", %{assemblage: assemblage, user: user}) do
-    %{
-      assemblage: Map.merge(
-        short(assemblage),
-        %{
-          file_ids: assemblage.files |> Enum.map(& &1.id),
-          tag_ids: assemblage.tags |> Enum.map(& &1.id)
-        }
-      ),
-      assemblies:
-        (assemblage.parent_assemblies ++ assemblage.child_assemblies)
-        |> Enum.map(&assembly/1),
-      assemblages:
-        (assemblage.parent_assemblages ++ assemblage.child_assemblages)
-        |> Enum.map(fn assemblage ->
-          Map.merge(
-            short(assemblage),
-            %{tag_ids: assemblage.tags |> Enum.map(& &1.id)}
-          )
-        end),
-      files:
-        assemblage.files
-        |> Enum.map(&file(&1, user)),
-      tags:
-        ([assemblage] ++ assemblage.parent_assemblages ++ assemblage.child_assemblages)
-        |> Enum.flat_map(& &1.tags)
-        |> Enum.map(&tag/1)
-    }
+    assemblage =
+      assemblage
+      |> Celeste.Repo.preload(:files)
+      |> Celeste.Repo.preload(:tags)
+      |> Celeste.Repo.preload(:child_assemblies)
+      |> Celeste.Repo.preload(:parent_assemblies)
+      |> Celeste.Repo.preload(parent_assemblages: :tags)
+      |> Celeste.Repo.preload(child_assemblages: :tags)
+
+    root =
+      %{}
+      |> attach_assemblages(assemblage.parent_assemblages)
+      |> attach_assemblages(assemblage.child_assemblages)
+      |> attach_assemblies(assemblage.parent_assemblies)
+      |> attach_assemblies(assemblage.child_assemblies)
+
+    {render_partial(assemblage, %{}), root}
+    |> embed(assemblage.tags, %{root: :tags, as: :tag_ids})
+    |> embed(assemblage.files, %{root: :files, as: :file_ids, with: %{user: user}})
+    |> finalize()
   end
 
-  def short(assemblage) do
+  defp attach_assemblages(root, assemblages) do
+    assemblages
+    |> Enum.map_reduce(root, fn assemblage, root ->
+      {render_partial(assemblage, %{}), root}
+      |> embed(assemblage.tags, %{root: :tags, as: :tag_ids})
+    end)
+    |> put_in_root(:assemblages)
+  end
+
+  defp attach_assemblies(root, assemblies) do
+    assemblies
+    |> Enum.map_reduce(root, fn assembly, root ->
+      {render_partial(assembly, %{}), root}
+    end)
+    |> put_in_root(:assemblies)
+  end
+
+  defp finalize({response, root}), do: Map.merge(root, %{assemblage: response})
+
+  defp embed({response, root}, relationship, opts \\ %{}) do
+    ids = Enum.map(relationship, & &1.id)
+    entities = Enum.map(relationship, &render_partial(&1, opts[:with]))
+
+    {Map.merge(response, %{opts[:as] => ids}),
+     deep_merge(root, %{opts[:root] => entities})}
+  end
+
+  defp put_in_root({list, root}, key), do: deep_merge(root, %{key => list})
+
+  defp deep_merge(map1, map2) do
+    Map.merge(map1, map2, fn _, v1, v2 -> v1 ++ v2 end)
+  end
+
+  defp render_partial(%Celeste.Assemblage{} = assemblage, _) do
     %{
       id: assemblage.id,
       name: assemblage.name,
       kind: assemblage.kind,
     }
   end
-
-  def file(file, user) do
+  defp render_partial(%CFile{id: id, mime: mime, path: path} = file, %{user: user}) do
     %{
-      id: file.id,
+      id: id,
       path: CFile.jwt(file, user),
-      mime: file.mime,
-      name: Path.basename(file.path)
+      mime: mime,
+      name: Path.basename(path)
     }
   end
-
-  def tag(tag) do
-    %{
-      id: tag.id,
-      key: tag.key,
-      value: tag.value
-    }
-  end
-
-  def assembly(assembly) do
+  defp render_partial(%Celeste.Assembly{} = assembly, _) do
     %{
       assemblage_id: assembly.assemblage_id,
       child_assemblage_id: assembly.child_assemblage_id,
       kind: assembly.kind
+    }
+  end
+
+  defp tag(tag) do
+    %{
+      id: tag.id,
+      key: tag.key,
+      value: tag.value
     }
   end
 end
